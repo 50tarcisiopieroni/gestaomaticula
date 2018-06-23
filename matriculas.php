c2d67f137f79285c2000300c90d47d446e7c4093
Tarcisio Pieroni <tarcisio.pieroni@gmail.com>
2018-06-23 00:29:36 -0300

    Gerando pagina de Matrícula

---
+++ matriculas.php
@@ -0,0 +1,10 @@
+<?php
+
+    require_once 'Base.php';
+   
+    $pagina = new Base();    
+    $pagina->SetMeio("<center>Ainda não há matriculas registradas no sistema</center>");        
+	$pagina->GerarHTML();
+
+    
+?>
